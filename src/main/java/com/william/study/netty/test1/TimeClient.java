a2457af670076c1a28a0f68852421fad0400af98
williamcheng1988 <williamcheng1988@gmail.com>
2017-06-04 22:46:26 +0800

    init

---
+++ src/main/java/com/william/study/netty/test1/TimeClient.java
@@ -0,0 +1,46 @@
+package com.william.study.netty.test1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class TimeClient {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("client start ...");
+        String host = "localhost";
+        int port = 8080;
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(workerGroup);
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new TimeClientHandler());
+
+                }
+            });
+            // 发起异步连接操作
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            workerGroup.shutdownGracefully();
+        }
+
+        System.out.println("client end ...");
+    }
+
+}
